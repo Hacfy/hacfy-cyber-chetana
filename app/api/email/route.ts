5aa7350fb7c175a252aa6a0731f585ef3812b291
sujaykumar <82111261+sujaykumar007@users.noreply.github.com>
2025-10-13 21:46:56 +0530

    add the double border

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -137,7 +137,7 @@ const generateCertificateHTML = (firstName: string, lastName: string, certificat
 
     .certificate {
       width: 1100px;       /* ~11in */
-      height: 820px;       /* ~8.2in */
+      height: 720px;       /* ~8.2in */
       border: 6px double #1e3a8a;
       border-radius: 8px;
       padding: 40px 60px;
